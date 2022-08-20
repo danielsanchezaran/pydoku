48db29173cb6d5bf5efa9c6f28f479c2b68db816
Daniel <danielsanchezaran@gmail.com>
2022-08-20 14:18:05 +0900

    [feat] brute force basedsolving implemented

--- sudoku_checker.py
+++ sudoku_checker.py
@@ -1,28 +1,112 @@
 from sudoku_generator import Board
+
+
 class BoardChecker:
-    def checkBoardRow(self, b : Board, row : int) -> bool:
+    def __init__(self, solving_method: str) -> None:
+        self.solving_method = solving_method
+
+    def checkBoardRow(self, b: Board, row: int) -> bool:
         checked_elems = b.dim * [False]
         row_to_check = b.get_row(row)
-
         for elem in row_to_check:
             if elem == 0:
-                return False
-            if checked_elems[elem.get_number()-1]:
-                return False
-            checked_elems[elem.get_number()-1] = True
-    def checkBoardColumn(self, b : Board, col: int) -> bool:
-        checked_elems = b.dim * [False]
-        col_to_check = b.get_col(col)
-        print(col_to_check)
-        for elem in col_to_check:
-            print("Test ", elem.get_number())
-            print()
-            if elem == 0:   
-                return False
+                continue
             if checked_elems[elem.get_number()-1]:
                 return False
             checked_elems[elem.get_number()-1] = True
         return True
 
+    def checkBoardColumn(self, b: Board, col: int) -> bool:
+        checked_elems = b.dim * [False]
+        col_to_check = b.get_col(col)
+        # print(col_to_check)
+        for elem in col_to_check:
+            if elem == 0:
+                continue
+            if checked_elems[elem.get_number()-1]:
+                return False
+            checked_elems[elem.get_number()-1] = True
+        return True
+
+    def checkBoardInnerSquare(self, b: Board, row: int, col: int) -> bool:
+        inner_sq_row, inner_sq_col = b.get_inner_square_row_col(
+            row, col)
+        inner_board = b.get_inner_square(inner_sq_row, inner_sq_col)
+        checked_elems = b.dim * [False]
+        inner_board_as_row_list = inner_board.get_board_as_row_list()
+        for elem in inner_board_as_row_list:
+            if elem == 0:
+                continue
+            if checked_elems[elem.get_number() - 1]:
+                return False
+            checked_elems[elem.get_number()-1] = True
+        return True
+
+    def checkCellisValid(self, b: Board, row: int, col: int) -> bool:
+        row_possible = self.checkBoardRow(b, row)
+        col_possible = self.checkBoardColumn(b, col)
+        inner_square_possible = self.checkBoardInnerSquare(b, row, col)
+        return row_possible and col_possible and inner_square_possible
+
+    def solve(self, b: "list[Board]") -> "list[Board]":
+        if (self.solving_method == "default"):
+            return self.solve_brute_force(b)
+
+    def solve_brute_force(self, b: "list[Board]") -> "list[Board]":
+        input_n = 1
+        row = 0
+        col = 0
+        self.brute_force_solver(b,row,col,input_n)
+        return b
+      
+    def brute_force_solver(self, b: "list[Board]", row: int, col: int, input_n: int) -> bool:
+        print("starting new solve at ")
+        print("row" , row, "col", col)
+        print("input", input_n)
+        print()
+        dim = b[0].dim
+        row_next = row + 1
+        col_next = col
+        input_n_next = 1
+
+        if row_next >= dim:
+            row_next = 0
+            col_next += 1
+        if col >= dim:
+            return True 
+        while col < dim and input_n <= dim:
+            if b[0].get_number(row, col) == 0:
+                b[0].input_number(row, col, input_n)
+                if not self.checkCellisValid(b[0], row, col):
+                    print("wrong input: ", input_n, " row" , row, " col", col)
+                    input_n += 1
+                    b[0].input_number(row, col, 0)
+                    continue
+                print("possibly right input: ", input_n, " row" , row, " col", col, "row_next, col_next,", row_next, col_next)
+                b[0].print_board()
+                if self.brute_force_solver(b, row_next, col_next, input_n_next):
+                    return True
+                input_n += 1
+                b[0].input_number(row, col, 0)
+                print("row" , row, "col", col)
+                print("input", input_n)
+                continue
+            return self.brute_force_solver(b, row_next, col_next, input_n_next)
+        return False
+
+
 if __name__ == "__main__":
     print()
+    b = Board(4)
+    checker = BoardChecker("default")
+
+    n = 1
+    for i in range(4):
+        b.input_number(i, 1, n)
+        n += 1
+
+    b.print_board()
+    b = checker.solve([b])
+    b[0].print_board()
+
+    print(checker.checkBoardColumn(b[0],2))
--- sudoku_generator.py
+++ sudoku_generator.py
@@ -4,89 +4,116 @@ import math
 
 
 class Cell:
-    def __init__(self) -> None:
-        self.number = 0
+    def __init__(self, number) -> None:
+        self.number = number
         pass
+
     def __repr__(self):
         if self.number == 0:
             return "X"
         return str(self.number)
+
     def __str__(self):
         if self.number == 0:
             return "X"
         return str(self.number)
+
     def __eq__(self, __o: object) -> bool:
         return self.number == __o
 
     def get_number(self) -> int:
         return self.number
-    def input_number(self, number : int) -> None:
+
+    def input_number(self, number: int) -> None:
         self.number = number
+
     def print_number(self) -> None:
         print(self.number)
 
+
 CellList = list[Cell]
 
+
 class Board:
-    def __init__(self, n : int) -> None:
+    def __init__(self, n: int) -> None:
         self.cell_matrix = []
         self.dim = n
-        self.dim_sqrt = int(math.sqrt(n)) #Should throw error if not int anyways
+        # Should throw error if not int anyways
+        self.dim_sqrt = int(math.sqrt(n))
         for _ in range(n):
-            temp_row =[]
+            temp_row = []
             for __ in range(n):
-                c = Cell()
+                c = Cell(0)
                 temp_row.append(c)
             self.cell_matrix.append(temp_row)
         pass
-    def get_row(self, n_row: int) -> CellList: 
+
+    def get_board_as_row_list(self) -> CellList:
+        out_list = []
+        for i in range(self.dim):
+            row_ = self.get_row(i)
+            out_list += row_
+        return out_list
+
+    def get_row(self, n_row: int) -> CellList:
         return self.cell_matrix[n_row]
-    def get_col(self, n_col: int) -> CellList: 
+
+    def get_col(self, n_col: int) -> CellList:
         return [x for x in [self.cell_matrix[y][n_col] for y in range(self.dim)]]
 
-    def get_inner_square_row_col()
-    def get_inner_square(self,n_square_row : int, n_square_col: int) -> Board: 
+    def get_inner_square_row_col(self, row: int, col: int) -> "list[int]":
+        return [int(row / self.dim_sqrt), int(col / self.dim_sqrt)]
+
+    def get_inner_square(self, n_square_row: int, n_square_col: int) -> Board:
         inner_square = []
-        for row in range(n_square_row*self.dim_sqrt,n_square_row *self.dim_sqrt+ self.dim_sqrt):
+        for row in range(n_square_row*self.dim_sqrt, n_square_row * self.dim_sqrt + self.dim_sqrt):
             temp_square = []
-            for col in range(n_square_col*self.dim_sqrt,self.dim_sqrt + n_square_col*self.dim_sqrt):
+            for col in range(n_square_col*self.dim_sqrt, self.dim_sqrt + n_square_col*self.dim_sqrt):
                 temp_square.append(self.cell_matrix[row][col])
             inner_square.append(temp_square)
         inner_square_board = Board(self.dim_sqrt)
         inner_square_board.cell_matrix = inner_square
         return inner_square_board
-    def input_number(self, row: int, col: int, n: int):
+
+    def input_number(self, row: int, col: int, n: int) -> None:
         self.cell_matrix[row][col].input_number(n)
+    
+    def get_number(self, row: int, col: int) -> Cell:
+        return self.cell_matrix[row][col]
+
     def print_board(self):
         print()
-        print( (6 * (self.dim-1) +1)* "-")
+        print((6 * (self.dim-1) + 1) * "-")
         for row in range(self.dim):
             curr_row = self.get_row(row)
-            for i , elem in enumerate(curr_row):
+            for i, elem in enumerate(curr_row):
                 print(elem, " ",  end='')
                 if (i + 1) % self.dim_sqrt == 0 and i < self.dim - 1:
                     print("|" + 2 * " ",  end='')
                 else:
-                     print(3 * " ",  end='')
+                    print(3 * " ",  end='')
             if (row + 1) % self.dim_sqrt == 0:
                 print()
-                print( (6 * (self.dim-1) + 1)* "-")
+                print((6 * (self.dim-1) + 1) * "-")
             else:
                 print()
 
 
-        
 if __name__ == "__main__":
     from sudoku_checker import *
-
-    b = Board(9)
-    checker = BoardChecker()
+    b = Board(4)
+    checker = BoardChecker("default")
     while True:
-        row = int(input ("Enter row number :"))
-        col = int(input ("Enter col number :"))
-        n = int(input ("Enter number :"))
-        b.input_number(row,col,n)
+        row = int(input("Enter row number :"))
+        col = int(input("Enter col number :"))
+        n = int(input("Enter number :"))
+        b.input_number(row, col, n)
+        print("Sub square row,col: ", b.get_inner_square_row_col(row, col))
         b.print_board()
         # f = b.get_inner_square(1,1)
         # f.print_board()
-        print(checker.checkBoardColumn(b,1))
+        print(checker.checkBoardColumn(b, 1))
+        print(b.get_board_as_row_list())
+        print("Inner square 0,0 is correct: ",
+              checker.checkBoardInnerSquare(b, 0, 0))
+        print("Cell is possible: ", checker.checkCellisValid(b, row, col))
