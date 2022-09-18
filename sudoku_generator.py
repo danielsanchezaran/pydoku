cb47e3c86dc04385b104d63bb5ba827bed5a680f
Daniel <danielsanchezaran@gmail.com>
2022-09-18 17:42:09 +0900

    [feat] generator of sudoku lists

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -2,8 +2,7 @@ from sudoku import Board
 from sudoku_checker import BoardChecker
 from copy import deepcopy
 from random import randint, sample
-import random
-
+import time
 
 class SudokuGenerator:
     def __init__(self, checker: BoardChecker) -> None:
@@ -19,12 +18,12 @@ class SudokuGenerator:
             return temp_sudoku
         return self.get_one_solution_sudoku(solved_sudoku)
 
-    def swap_digits(self, n_1: int, n_2: int, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
+    def swap_digits(self, n_1: int, n_2: int, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
         pos_1 = -1
         pos_2 = -1
-        for i in range(board[0].dim):
-            row = board[0].get_row(i)
+        for i in range(board.dim):
+            row = board.get_row(i)
             for j in range(len(row)):
                 if row[j] == n_1:
                     pos_1 = j
@@ -33,66 +32,68 @@ class SudokuGenerator:
                 if pos_1 > 0 and pos_2 > 0:
                     break
             row[pos_1], row[pos_2] = row[pos_2], row[pos_1]
-            out_board.cell_matrix[i] = row
+            board.cell_matrix[i] = row
             pos_1 = -1
             pos_2 = -1
-        return out_board
+        return board
 
-    def swap_random_digits(self, board: "list[Board]") -> Board:
-        n_1, n_2 = sample(range(1, board[0].dim - 1), 2)
+    def swap_random_digits(self, board: Board) -> Board:
+        n_1, n_2 = sample(range(1, board.dim - 1), 2)
         return self.swap_digits(n_1, n_2, board)
 
-    def swap_rows(self, row_1: int, row_2: int, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
-        out_board.cell_matrix[row_1], out_board.cell_matrix[row_2] = out_board.cell_matrix[row_2], out_board.cell_matrix[row_1]
-        return out_board
+    def swap_rows(self, row_1: int, row_2: int, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
+        board.cell_matrix[row_1], board.cell_matrix[row_2] = board.cell_matrix[row_2], board.cell_matrix[row_1]
+        return board
 
-    def shuffle_large_rows(self, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
-        for original_row in range(out_board.dim_sqrt):
-            target_row = randint(0, out_board.dim_sqrt - 1)
-            for n in range(out_board.dim_sqrt):
-                out_board = self.swap_rows(
-                    (original_row * out_board.dim_sqrt) + n, (target_row * out_board.dim_sqrt) + n, [out_board])
-        return out_board
+    def shuffle_large_rows(self, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
+        for original_row in range(board.dim_sqrt):
+            target_row = randint(0, board.dim_sqrt - 1)
+            for n in range(board.dim_sqrt):
+                board = self.swap_rows(
+                    (original_row * board.dim_sqrt) + n, (target_row * board.dim_sqrt) + n, board)
+        return board
 
-    def shuffle_large_cols(self, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
-        out_board.transpose_board()
-        out_board = self.shuffle_large_rows([out_board])
-        out_board.transpose_board()
-        return out_board
+    def shuffle_large_cols(self, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
+        board.transpose_board()
+        board = self.shuffle_large_rows(board)
+        board.transpose_board()
+        return board
 
-    def shuffle_rows(self, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
-        for i in range(out_board.dim):
-            block_num = int(i / out_board.dim_sqrt)
-            n = randint(0, out_board.dim_sqrt - 1)
-            out_board = self.swap_rows(
-                i, block_num * out_board.dim_sqrt + n, [out_board])
-        return out_board
+    def shuffle_rows(self, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
+        for i in range(board.dim):
+            block_num = int(i / board.dim_sqrt)
+            n = randint(0, board.dim_sqrt - 1)
+            board = self.swap_rows(
+                i, block_num * board.dim_sqrt + n, board)
+        return board
 
-    def shuffle_cols(self, board: "list[Board]") -> Board:
-        out_board = deepcopy(board[0])
-        out_board.transpose_board()
-        out_board = self.shuffle_rows([out_board])
-        out_board.transpose_board()
-        return out_board
+    def shuffle_cols(self, board: Board) -> Board:
+        # board[0] = deepcopy(board[0])
+        board.transpose_board()
+        board = self.shuffle_rows(board)
+        board.transpose_board()
+        return board
 
-    def generate_sudoku_list(self, mother_board: "list[Board]", n_sudokus : int)  -> "list[Board]":
-        # mother_board = self.get_one_solution_sudoku(starting_board)
-        out_list = [mother_board[0]]
-
-        while len(out_list) < n_sudokus: 
-            for i in range(len(out_list)):
-                new_elem = self.randomnize_board([out_list[i]])
-                out_list.append(new_elem)
-                if len(out_list) >= n_sudokus:
+    def generate_sudoku_list(self, mother_board: "Board", n_sudokus : int)  -> Board:
+        out_list = []
+        out_list.append(deepcopy(mother_board))
+        created_sudokus = 1
+        while created_sudokus < n_sudokus: 
+            len_of_list = len(out_list)
+            for i in range(len_of_list):
+                out_list.append(self.randomnize_board(deepcopy(out_list[i])))
+                created_sudokus += 1
+                if created_sudokus >= n_sudokus:
                     break
         return out_list
 
-    def randomnize_board(self, starting_board: "list[Board]") -> Board:
+    def randomnize_board(self, starting_board: Board) -> Board:
         n_function = randint(0, 4)
+        starting_board.print_board()
         if n_function == 0:
             return self.swap_random_digits(starting_board)
         if n_function == 1:
@@ -128,19 +129,19 @@ if __name__ == "__main__":
 
     generator = SudokuGenerator(checker)
     print("Swapping 9 by 5")
-    new_board = generator.swap_digits(9, 5, sol)
+    new_board = generator.swap_digits(9, 5, sol[0])
     new_board.print_board()
 
     print("Shuffled Cols")
-    new_board = generator.shuffle_cols([new_board])
+    new_board = generator.shuffle_cols(new_board)
     new_board.print_board()
 
     print("Shuffled Large Row")
-    new_board = generator.shuffle_large_rows([new_board])
+    new_board = generator.shuffle_large_rows(new_board)
     new_board.print_board()
 
     print("Shuffled Large Col")
-    new_board = generator.shuffle_large_cols([new_board])
+    new_board = generator.shuffle_large_cols(new_board)
     new_board.print_board()
 
     one_solution_sudoku = generator.get_one_solution_sudoku(sol)
@@ -152,8 +153,11 @@ if __name__ == "__main__":
     mother_sudoku[0].print_board()
     
     print("Getting a list of 10 one soultion sudokus")
-    list_of_sudokus = generator.generate_sudoku_list(mother_sudoku,10)
+    t_start = time.time()
+    list_of_sudokus = generator.generate_sudoku_list(mother_sudoku[0],100)
+    print("elapsed time is ",time.time() - t_start)
+    print("Total Sudokus: ", len(list_of_sudokus))
 
-    for i in range(len(list_of_sudokus)):
+    for i in range(10):
         list_of_sudokus[i].print_board()
 
